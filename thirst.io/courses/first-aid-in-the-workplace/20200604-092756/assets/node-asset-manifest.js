b6638817bdb4440591321c853c646190ac6a2b61
Fred Thompson <fred.thompson@buildempire.co.uk>
2020-06-04 10:32:14 +0100

    Added the latest version of the first aid course.

---
+++ thirst.io/courses/first-aid-in-the-workplace/20200604-092756/assets/node-asset-manifest.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+define('dummy/config/node-asset-manifest', function() {
+  return {
+    default: {"bundles":{"thirst-editor":{"assets":[{"uri":"/engines-dist/thirst-editor/config/environment-c760d4511101074a7dc3b9ab9d129384.js","type":"js"}]}}}
+  };
+});
