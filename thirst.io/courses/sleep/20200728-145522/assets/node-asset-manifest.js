f51d1fdcd879be6f704ac097bd4b6f035bc79f90
Fred Thompson <fred.thompson@buildempire.co.uk>
2020-07-28 16:20:42 +0100

    Added a new version.

---
+++ thirst.io/courses/sleep/20200728-145522/assets/node-asset-manifest.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+define('dummy/config/node-asset-manifest', function() {
+  return {
+    default: {"bundles":{"thirst-editor":{"assets":[{"uri":"/engines-dist/thirst-editor/config/environment-4a6780f74b1b127e45c79b980b98ca40.js","type":"js"}]}}}
+  };
+});
